856343120e6f4e3072a37635def7efa7aa8ef69f
Cleiton Duarte Goncalves <cdgoncalves@fazenda.ms.gov.br>
2022-10-15 18:17:48 -0400

    criado component nova transferencia

---
+++ src/app/nova-transferencia/app.nova-transferencia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-nova-transferencia',
+  templateUrl:'./app.nova-transferencia.component.html',
+  styleUrls:['./app.nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent{
+
+}
